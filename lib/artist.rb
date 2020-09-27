d4ebf3a9fb1f9cf46a83b239b353228c8611dc3e
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-27 03:02:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    @@all.detect {|artist| artist.name == name || self.new(name)}
+    @@self.all.detect {|artist| artist.name == name || self.new(name)}
   end
 
   def print_songs
